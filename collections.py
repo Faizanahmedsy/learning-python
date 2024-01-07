47e75de2f07bbd35e78c84db8ddcbcf7c95d9d14
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-08 01:11:01 +0530

    fix: fixed incorrect notes about list

--- collections.py
+++ collections.py
@@ -2,8 +2,13 @@
 fruits = [4, True, "hi"]
 
 
-# Append method is used to push elements in the array, the new elements are added in the last
+# Append method is used to push an element in the array, the new element is added in the last
 fruits.append("This was added in the last")
+
+
+
+# Extend method is used to push an array in side another array, the new array is added after last element of first array 
+fruits.extend([1,2,3,4])
 
 
 print(fruits)
