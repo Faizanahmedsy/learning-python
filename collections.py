e6ec8af2922eea2eabe43328b47058130152528b
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-08 01:15:25 +0530

    Learned pop method to remove elements from the list

--- collections.py
+++ collections.py
@@ -2,13 +2,26 @@
 fruits = [4, True, "hi"]
 
 
-# Append method is used to push an element in the array, the new element is added in the last
+# Append method is used to push an element in the list, the new element is added in the last
 fruits.append("This was added in the last")
 
 
 
-# Extend method is used to push an array in side another array, the new array is added after last element of first array 
-fruits.extend([1,2,3,4])
+# Extend method is used to push an list in side another list, the new list is added after last element of first list 
+# //It loops of all this elemets and just appends them in to the original list 
+fruits.extend([1,2,3,"This will be removed because of pop method"])
+
+
+
+# This removes the last elemet of the list
+fruits.pop()
+
+
+
+
+# pop method can also take index as an argument, it will remove the elemet in that perticular index
+
+fruits.pop(3)
 
 
 print(fruits)
