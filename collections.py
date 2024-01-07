5a705bfe12189ffc3b105a1d9e57ba1a49a65d41
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-08 01:08:47 +0530

    learned list and push methods

---
+++ collections.py
@@ -0,0 +1,12 @@
+# This is a list which is a ordered collection, elements of the list can be of any datatype
+fruits = [4, True, "hi"]
+
+
+# Append method is used to push elements in the array, the new elements are added in the last
+fruits.append("This was added in the last")
+
+
+print(fruits)
+
+
+print("There are " + str(len(fruits)) + " Fruits")
