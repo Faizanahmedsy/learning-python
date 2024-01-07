b84337fd93d3ada335abffda5b5f20dd7609427a
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-08 01:01:00 +0530

    Learned If else statemetns

--- hello.py
+++ hello.py
@@ -1,1 +1,28 @@
-print("Hello world")
+
+# name = "Faizan"
+# surname = "76"
+# # print(name + " " + surname)
+
+# # for i in range(10):
+# #     print(i)
+
+
+
+# Ask to input a value and store that value in result variable
+result = input("Enter your passkey: ")
+
+result = result.lower()
+
+
+
+
+if result == "vrockpokey":
+    name = "Faizan"
+    print("Hello " + name + " Sir Lets take off")
+else:
+    print("You are not authorised")
+
+
+
+
+
